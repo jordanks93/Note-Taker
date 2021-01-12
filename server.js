59fffc048384c6962bdfb853a9640067ef9ea287
Jordanks93 <69221166+jordanks93@users.noreply.github.com>
2021-01-12 18:06:05 -0500

    updated unreachable console log for server setup failure

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ require("./routes/htmlRoutes")(app);
 // starts the server
 app.listen(PORT, (err) => {
   if(err) {
-    throw(err);
     console("Server setup failed.")
+    throw(err);
   } else {
     console.log("App listening on PORT: " + PORT);
   }
